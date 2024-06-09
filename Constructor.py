d9a63e99b00db95cad391d184658f6862d1644d1
creater toro <bablupandey446@gmil.com>
2024-06-09 11:30:38 +0530

    uisng the constructor in the class

---
+++ Constructor.py
@@ -0,0 +1,9 @@
+# here we uisng the constructor 
+class Employee:
+    def __init__(self):
+        self.salary = 22000
+        self.age  = 21
+
+e1 = Employee()
+e2 = Employee()
+print(e1.__dict__)
