b923f245057415bc46e8fd0401aa21c9969a5367
creater toro <bablupandey446@gmil.com>
2024-06-10 10:14:22 +0530

    Acessing the method using the disply funtion ()

---
+++ Acessing the method.py
@@ -0,0 +1,23 @@
+# here we accesing the method 
+# here we access class members 
+class Employee :
+   def __init__(self,sal,age):
+        self.salary = sal
+        self.age = age 
+
+
+   def display(self):
+       print(f'salary is {self.salary} and age is {self.age}')
+
+
+e1 = Employee(24000,21)
+e2 = Employee(26000,26)
+
+# # here we accesing the mathod and attribute 
+# print(e1.salary)
+# e1.salary = 34000000 # updating attribute 
+# print(e1.salary)
+
+# accesing the mathod 
+e2.display()
+
