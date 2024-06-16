e3328d95039b3682618b7129243f365c2028d80f
creater toro <bablupandey446@gmil.com>
2024-06-16 13:27:24 +0530

    In this add finally keyword and

---
+++ Exception Handling/Finally_eh.py
@@ -0,0 +1,9 @@
+# The finally block, if specified, will be executed regardless if the try block raises an error or not
+try :
+ print(x)
+
+except :
+ print('Something went wrong')
+
+finally:
+ print("The 'try excption 'is finished")
