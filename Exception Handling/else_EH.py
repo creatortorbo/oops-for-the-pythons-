19864ca237143196f19fbb5d6e97f7b71b9c0c4a
creater toro <bablupandey446@gmil.com>
2024-06-16 13:14:10 +0530

    In Exception handiling uisng the else statement

---
+++ Exception Handling/else_EH.py
@@ -0,0 +1,9 @@
+# here using the else in the exception handing 
+try :
+ print('my name is bablu')
+
+except :
+ print('Something went wrong')
+
+else :
+ print('nothing went wrong in the code (No error)')
