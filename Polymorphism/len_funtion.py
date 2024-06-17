eb0c62b91b8800afaa261f2224558dbeb2bcebe3
creater toro <bablupandey446@gmil.com>
2024-06-17 07:05:55 +0530

    using len funtion on string

---
+++ Polymorphism/len_funtion.py
@@ -0,0 +1,4 @@
+# here using the len funtion for cal the length of the char 
+my_name = 'bablu kumar pandey'
+print(f'printing the my_name',{my_name})
+print('printing the len of string :',len(my_name))
